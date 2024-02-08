4750974eaae62c88f3219c31417de61fb542572d
Mattias Karlsson <matkar@wcom.se>
2024-02-08 08:34:54 +0100

    Update ARI

--- Azure/build.ps1
+++ Azure/build.ps1
@@ -1,2 +1,5 @@
+if (-not (Test-Path -Path "./AzureDocs/input")) {
+    New-Item -ItemType Directory -Path "./AzureDocs/input" | Out-Null
+}
 'dotnet ari inventory $env:AZURE_TENANT_ID ./AzureDocs/input --skip-tenant-overview'
 dotnet ari inventory $env:AZURE_TENANT_ID ./AzureDocs/input --skip-tenant-overview

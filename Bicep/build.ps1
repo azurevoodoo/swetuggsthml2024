2cf2bf4ffa8e2a413ac2af66d3bfe0b9f1dec733
Mattias Karlsson <matkar@wcom.se>
2024-02-08 08:45:23 +0100

    Ensure bicep inital path exists

--- Bicep/build.ps1
+++ Bicep/build.ps1
@@ -1,2 +1,5 @@
+if (-not (Test-Path -Path ./BicepDocs/input)) {
+    New-Item -ItemType Directory -Path ./BicepDocs/input | Out-Null
+}
 'dotnet bri inventory $ENV:AZURE_CONTAINER_REGISTRY ./BicepDocs/input'
 dotnet bri inventory $ENV:AZURE_CONTAINER_REGISTRY ./BicepDocs/input
